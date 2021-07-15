b2ed14ae99284ef4e947f0868c52078c1bf371e3
MdNadimHossain <getovic@gmail.com>
2021-07-15 21:22:24 +1000

    [DDIDO-594] Updated dev-tools commit.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=ca49389288fdacb2bd9c3d41d66d8b1a037ee472
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
