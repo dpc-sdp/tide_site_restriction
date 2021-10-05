a248cf111f85496963072714cdb2f3ed4993ef95
MdNadimHossain <getovic@gmail.com>
2021-10-05 18:04:27 +1100

    Updated more lint issues.

--- src/TideSiteRestrictionServiceProvider.php
+++ src/TideSiteRestrictionServiceProvider.php
@@ -27,7 +27,7 @@ class TideSiteRestrictionServiceProvider extends ServiceProviderBase implements 
     $definition = $container->getDefinition('access_check.node.revision');
     $definition->setClass('Drupal\tide_site_restriction\Access\RevisionAccessCheck')
       ->setArguments([
-        new Reference('entity_type.manager'), 
+        new Reference('entity_type.manager'),
         new Reference('tide_site_restriction.helper'),
       ]);
   }
