75bd46e94931fae4915f748b442c3bff907351cd
vincent-gao <vincent.gao@dpc.vic.gov.au>
2022-11-08 15:15:09 +1100

    [SDPAP-6697] Add 2 layers of permissions to views (#41)
    * Add 2 layers of permissions to views
    
    * Fixes a lint

--- src/Access/SiteAccessRouteCheck.php
+++ src/Access/SiteAccessRouteCheck.php
@@ -8,7 +8,9 @@ use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\node\NodeInterface;
 use Drupal\tide_site_restriction\Helper;
+use Drupal\user\Entity\User;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Routing\Route;
 
 /**
  * Checks access for routers when tide_site_restriction module gets enabled.
@@ -63,6 +65,34 @@ class SiteAccessRouteCheck implements AccessInterface {
         ->cachePerUser();
     }
     return $result;
+  }
+
+  /**
+   * Check if the user can access the route attached to a view.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account.
+   * @param \Symfony\Component\Routing\Route $route
+   *   The node.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   A result.
+   */
+  public function viewsAccess(AccountInterface $account, Route $route) {
+    if ($this->helper->canBypassRestriction($account)) {
+      return AccessResult::allowed();
+    }
+    $permission = $route->getOption('_views_permission');
+    $sites = array_keys(array_filter($route->getOption('_views_sites')));
+    if (empty($sites)) {
+      return AccessResult::neutral();
+    }
+    $user_sites = $this->helper->getUserSites(User::load($account->id()));
+    $assigned_site = array_intersect_key($user_sites, array_flip($sites));
+    if ($account->hasPermission($permission) && !empty($assigned_site)) {
+      return AccessResult::allowed();
+    }
+    return AccessResult::forbidden();
   }
 
 }
